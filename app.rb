e3cf36364339d57b3ddc2ad2da3029cb19195489
Jack Madden <jck13mad@gmail.com>
2020-06-06 21:28:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    @number.times_do
+    @number.times do
       puts @phrase
     end
   end
