34d637eab4e3f34a7f1473b31d2b1307b33c1c6d
Jack Madden <jck13mad@gmail.com>
2020-06-06 21:42:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -41,7 +41,13 @@ class App < Sinatra::Base
     
     case @operation
     when 'add'
-      
+      (@number1 + @number2).to_s
+    when 'subtract'
+      (@number1 - @number2).to_s
+    when 'multiply'
+      (@number1 * @number2).to_s
+    when 'divide'
+      (@number1 / @number2).to_s
   
   
   
