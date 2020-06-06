665a3ac3eed87217d8f1bf81cf3754798373668d
Jack Madden <jck13mad@gmail.com>
2020-06-06 21:38:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
     @word4 = params[:word4]
     @word5 = params[:word5]
     
-    final_string = "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
+    final_string = "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
     final_string
   end
     
